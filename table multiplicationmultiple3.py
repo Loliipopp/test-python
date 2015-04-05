0938fd2c08aabb4e2cf21f869817db69b54e94f3
Loliipopp <loliipopp@hotmail.fr>
2015-04-05 16:01:45 +0200

    fichier ajouté!

---
+++ table multiplicationmultiple3.py
@@ -0,0 +1,25 @@
+def table_de(chiffre,multiple):
+    res=0
+    i=1
+    while i<10:
+        res=i*chiffre
+        i=i+1
+        if (res%multiple) == 0:
+            print(res,"*")
+        else:
+            print(res)
+        
+def table_conversion_euro_dollar():
+    eu=1
+    do=1.65
+    while eu < 16384:
+        print(eu,"euro(s) =",do,"dollar(s)")
+        eu=eu*2
+        do=do*2
+
+def suite_triple(chiffre):
+    i=1
+    while i < 12:
+        print(chiffre)
+        chiffre=chiffre*3
+        i=i+1
